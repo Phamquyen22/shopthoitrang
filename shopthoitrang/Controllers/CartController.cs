6020718b8b952d1e786d6cdc4a82fb3bfcfb4409
Phamquyen22 <125226629+Phamquyen22@users.noreply.github.com>
2024-04-06 09:19:38 +0700

    6/4/2024
    làm chức năng thanh toán vnpay

--- shopthoitrang/Controllers/CartController.cs
+++ shopthoitrang/Controllers/CartController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
+
+using System.Configuration;
 using System.Linq;
-using System.Web;
+
 using System.Web.Mvc;
 using System.Web.Security;
 using shopthoitrang.Models;
+using shopthoitrang.Models.payment;
 namespace shopthoitrang.Controllers
 {
     public class CartController : Controller
@@ -132,7 +134,110 @@ namespace shopthoitrang.Controllers
                 return Json(new { success = false, redirectUrl = "/Home/Login" });
             }
         }
+        public ActionResult xoa_cart (int id_pro)
+        {
+            return View();
+        }
 
+
+        public ActionResult checkout()
+        {
+
+            return View();
+        }
+
+        //thanh toan vnpay
+        public void thanhtoan()
+        {
+
+            string vnp_Returnurl = ConfigurationManager.AppSettings["vnp_Returnurl"]; //URL nhan ket qua tra ve 
+            string vnp_Url = ConfigurationManager.AppSettings["vnp_Url"]; //URL thanh toan cua VNPAY 
+            string vnp_TmnCode = ConfigurationManager.AppSettings["vnp_TmnCode"]; //Ma định danh merchant kết nối (Terminal Id)
+            string vnp_HashSecret = ConfigurationManager.AppSettings["vnp_HashSecret"]; //Secret Key
+
+            //Get payment input
+
+            long OrderId = DateTime.Now.Ticks; // Giả lập mã giao dịch hệ thống merchant gửi sang VNPAY
+            long Amount = 10000; // Giả lập số tiền thanh toán hệ thống merchant gửi sang VNPAY 100,000 VND
+            string Status = "0"; //0: Trạng thái thanh toán "chờ thanh toán" hoặc "Pending" khởi tạo giao dịch chưa có IPN
+            DateTime CreatedDate = DateTime.Now;
+            //Save order to db
+
+            //Build URL for VNPAY
+            VnPayLibrary vnpay = new VnPayLibrary();
+
+            vnpay.AddRequestData("vnp_Version", VnPayLibrary.VERSION);
+            vnpay.AddRequestData("vnp_Command", "pay");
+            vnpay.AddRequestData("vnp_TmnCode", vnp_TmnCode);
+            vnpay.AddRequestData("vnp_Amount", (Amount * 100).ToString()); //Số tiền thanh toán. Số tiền không mang các ký tự phân tách thập phân, phần nghìn, ký tự tiền tệ. Để gửi số tiền thanh toán là 100,000 VND (một trăm nghìn VNĐ) thì merchant cần nhân thêm 100 lần (khử phần thập phân), sau đó gửi sang VNPAY là: 10000000
+            vnpay.AddRequestData("vnp_BankCode", "VNBANK");
+            vnpay.AddRequestData("vnp_CreateDate", CreatedDate.ToString("yyyyMMddHHmmss"));
+            vnpay.AddRequestData("vnp_CurrCode", "VND");
+            vnpay.AddRequestData("vnp_IpAddr", Utils.GetIpAddress());
+            vnpay.AddRequestData("vnp_Locale", "vn");
+            vnpay.AddRequestData("vnp_OrderInfo", "Thanh toan don hang:" + OrderId);
+            vnpay.AddRequestData("vnp_OrderType", "other"); //default value: other
+
+            vnpay.AddRequestData("vnp_ReturnUrl", vnp_Returnurl);
+            vnpay.AddRequestData("vnp_TxnRef", OrderId.ToString()); // Mã tham chiếu của giao dịch tại hệ thống của merchant. Mã này là duy nhất dùng để phân biệt các đơn hàng gửi sang VNPAY. Không được trùng lặp trong ngày
+
+
+            string paymentUrl = vnpay.CreateRequestUrl(vnp_Url, vnp_HashSecret);
+
+            Response.Redirect(paymentUrl);
+        }
+        public ActionResult order()
+        {
+            string vnp_HashSecret = ConfigurationManager.AppSettings["vnp_HashSecret"]; // Chuỗi bí mật
+            var vnpayData = Request.QueryString;
+            VnPayLibrary vnpay = new VnPayLibrary();
+
+            foreach (string s in vnpayData)
+            {
+                // Lấy tất cả dữ liệu từ query string
+                if (!string.IsNullOrEmpty(s) && s.StartsWith("vnp_"))
+                {
+                    vnpay.AddResponseData(s, vnpayData[s]);
+                }
+            }
+
+            // Lấy các thông tin từ phản hồi VNPAY
+            long orderId = Convert.ToInt64(vnpay.GetResponseData("vnp_TxnRef"));
+            long vnpayTranId = Convert.ToInt64(vnpay.GetResponseData("vnp_TransactionNo"));
+            string vnp_ResponseCode = vnpay.GetResponseData("vnp_ResponseCode");
+            string vnp_TransactionStatus = vnpay.GetResponseData("vnp_TransactionStatus");
+            string vnp_SecureHash = Request.QueryString["vnp_SecureHash"];
+            string TerminalID = Request.QueryString["vnp_TmnCode"];
+            long vnp_Amount = Convert.ToInt64(vnpay.GetResponseData("vnp_Amount")) / 100;
+            string bankCode = Request.QueryString["vnp_BankCode"];
+
+            bool checkSignature = vnpay.ValidateSignature(vnp_SecureHash, vnp_HashSecret);
+            if (checkSignature)
+            {
+                if (vnp_ResponseCode == "00" && vnp_TransactionStatus == "00")
+                {
+                    // Thanh toán thành công
+                    ViewBag.Message = "Giao dịch được thực hiện thành công. Cảm ơn quý khách đã sử dụng dịch vụ";
+                }
+                else
+                {
+                    // Thanh toán không thành công
+                    ViewBag.Message = "Có lỗi xảy ra trong quá trình xử lý.Mã lỗi: " + vnp_ResponseCode;
+                }
+                ViewBag.TmnCode = "Mã Website (Terminal ID):" + TerminalID;
+                ViewBag.TxnRef = "Mã giao dịch thanh toán:" + orderId.ToString();
+                ViewBag.VnpayTranNo = "Mã giao dịch tại VNPAY:" + vnpayTranId.ToString();
+                ViewBag.Amount = "Số tiền thanh toán (VND):" + vnp_Amount.ToString();
+                ViewBag.BankCode = "Ngân hàng thanh toán:" + bankCode;
+            }
+            else
+            {
+                ViewBag.Message = "Có lỗi xảy ra trong quá trình xử lý";
+            }
+
+            return View();
+
+        }
 
     }
 }
